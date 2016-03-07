cae2e4306987c2262b82e7d2ed201dea98999156
JeromeTroy <jrtroy@udel.edu>
2016-03-07 15:02:13 -0500

    Finished Testing

---
+++ PS2/src/main/java/base/Triangle.java
@@ -0,0 +1,68 @@
+package base;
+
+public class Triangle extends GeometricObject{
+	
+	private double side1 = 1.0;
+	private double side2 = 1.0;
+	private double side3 = 1.0;
+	
+	/**
+	 * Creates default triangle
+	 */
+	public Triangle(){}
+	
+	/**
+	 * Sets the triangle lengths to specified lengths
+	 * @param s1
+	 * @param s2
+	 * @param s3
+	 */
+	public Triangle(double s1, double s2, double s3){
+		this.side1 = s1;
+		this.side2 = s2;
+		this.side3 = s3;
+	}
+	
+	public double getSide1(){
+		return this.side1;
+	}
+	public double getSide2(){
+		return this.side2;
+	}
+	public double getSide3(){
+		return this.side3;
+	}
+	
+	/**
+	 * Gets the area of the triangle
+	 * Uses formula of only sides to determine area
+	 * @return
+	 */
+	@Override
+	public double getArea(){
+		double a = this.side1;
+		double b = this.side2;
+		double c = this.side3;
+		return Math.sqrt((a+b+c)*(b+c-a)*(c+a-b)*(a+b-c))/4;
+	}
+	
+	/**
+	 * Gets the perimeter of the triangle
+	 * @return
+	 */
+	@Override
+	public double getPerimeter(){
+		return this.side1+this.side2+this.side3;
+	}
+	
+	/**
+	 * returns a description of the triangle:
+	 * all 3 side lengths, fill color, whether is filled or not
+	 * @return
+	 */
+	@Override
+	public String toString(){
+		return "Sides: " + side1+", "+side2+", "+side3+"\nColor: "+this.getColor()+"\nFilled: "+this.isFilled();
+	}
+
+}
---
+++ PS2/src/test/java/base/TriangleTest.java
@@ -0,0 +1,41 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TriangleTest {
+	
+	@Test
+	public void testDefault() {
+		Triangle T1 = new Triangle();
+		assertTrue(T1.getSide1() == 1);
+		assertTrue(T1.getSide2() == 1);
+		assertTrue(T1.getSide3() == 1);
+	}
+	@Test
+	public void testNew(){
+		Triangle T2 = new Triangle(3,4,5);
+		assertTrue(T2.getSide1() == 3);
+		assertTrue(T2.getSide2() == 4);
+		assertTrue(T2.getSide3() == 5);
+	}
+	
+	@Test
+	public void testPerimeter(){
+		Triangle T3 = new Triangle();
+		assertTrue(T3.getPerimeter() == 3);
+	}
+	
+	@Test 
+	public void testArea(){
+		Triangle T4 = new Triangle(3,4,5);
+		Triangle T5 = new Triangle();
+		assertTrue(T4.getArea() == 6);
+		assertTrue(T5.getArea() == Math.sqrt(3)/4);
+	}
+}
